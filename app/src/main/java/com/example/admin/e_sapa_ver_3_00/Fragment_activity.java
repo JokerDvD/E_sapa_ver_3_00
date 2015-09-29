22b96d5d194bf7a2e0b5d3a7fe84c11af42f6244
Slamaka Cool <shaqyrslam@gmail.com>
2015-09-29 10:47:53 +0400

    add NaviigationDrawer and add toolbar

--- app/src/main/java/com/example/admin/e_sapa_ver_3_00/Fragment_activity.java
+++ app/src/main/java/com/example/admin/e_sapa_ver_3_00/Fragment_activity.java
@@ -3,19 +3,57 @@ package com.example.admin.e_sapa_ver_3_00;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.Nameable;
+
 public class Fragment_activity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private DrawerBuilder builder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment_activity);
-        ViewPager pager=(ViewPager)findViewById(R.id.pager);
-        pager.setAdapter(new MyAdapter(this,getSupportFragmentManager()));
+        ViewPager pager = (ViewPager) findViewById(R.id.pager);
+        pager.setAdapter(new MyAdapter(this, getSupportFragmentManager()));
 //        pager.setPageTransformer(true, new ZoomOutPageTransformer());
-        pager.setPageTransformer(true,new DepthPageTransformer());
+        pager.setPageTransformer(true, new DepthPageTransformer());
+        setToolbar();
+        builder=new DrawerBuilder().withActivity(this).withToolbar(toolbar).addDrawerItems(
+                new SecondaryDrawerItem().withIcon(R.drawable.setting_icon).withName(R.string.action_settings).setEnabled(true)
+        )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(AdapterView<?> adapterView, View view, int i, long l, IDrawerItem drawerItem) {
 
+                        if (drawerItem != null) {
+                            if (drawerItem instanceof Nameable) {
+                                toolbar.setTitle(((Nameable)
+                                        drawerItem).getNameRes());
+                            }
+                        }
+
+                        return false;
+                    }
+                });
+
+        Drawer drawer=builder.build();
+
+    }
+
+    private void setToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
