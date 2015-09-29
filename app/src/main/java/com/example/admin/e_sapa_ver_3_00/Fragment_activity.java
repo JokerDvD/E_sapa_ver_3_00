9949b934dc82f5979db006f455924663e73a55d6
Slamaka Cool <shaqyrslam@gmail.com>
2015-09-29 09:47:00 +0400

    add viewPager

--- app/src/main/java/com/example/admin/e_sapa_ver_3_00/Fragment_activity.java
+++ app/src/main/java/com/example/admin/e_sapa_ver_3_00/Fragment_activity.java
@@ -1,16 +1,18 @@
 package com.example.admin.e_sapa_ver_3_00;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-
 public class Fragment_activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment_activity);
+        ViewPager pager=(ViewPager)findViewById(R.id.pager);
+        pager.setAdapter(new MyAdapter(getSupportFragmentManager()));
     }
 
     @Override
---
+++ app/src/main/java/com/example/admin/e_sapa_ver_3_00/ValidateTabacco.java
@@ -0,0 +1,39 @@
+package com.example.admin.e_sapa_ver_3_00;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class ValidateTabacco extends Fragment {
+
+    private int pageNumber;
+
+    public static ValidateTabacco newInstance(int page) {
+        ValidateTabacco fragment = new ValidateTabacco();
+        Bundle args=new Bundle();
+        args.putInt("num", page);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public ValidateTabacco() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        pageNumber = getArguments() != null ? getArguments().getInt("num") : 1;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View result=inflater.inflate(R.layout.fragment_validate_tabacco, container, false);
+        TextView pageHeader=(TextView)result.findViewById(R.id.displayText);
+        pageHeader.setText("Фрагмент " + String.valueOf(pageNumber+1));
+        return result;
+    }
+}
