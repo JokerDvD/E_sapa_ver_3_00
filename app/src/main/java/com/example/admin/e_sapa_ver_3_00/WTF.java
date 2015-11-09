81306f505507abf4377aa1f35e8f489463def280
Slamaka Cool <shaqyrslam@gmail.com>
2015-11-09 15:07:39 +0600

    check point

--- app/src/main/java/com/example/admin/e_sapa_ver_3_00/WTF.java
+++ app/src/main/java/com/example/admin/e_sapa_ver_3_00/WTF.java
@@ -2,18 +2,39 @@ package com.example.admin.e_sapa_ver_3_00;
 
 import android.app.Fragment;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 
-public class WTF extends Fragment {
-    @Nullable
+public class WTF extends Fragment implements View.OnClickListener {
+
+
+    Button loginBtn/*, signupButton*/;
+    EditText loginUsername,loginPassword;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_wt, container, false);
 
+        loginUsername=(EditText) view.findViewById(R.id.loginUsername);
+        loginPassword=(EditText)view.findViewById(R.id.loginPassword);
+
+        loginBtn=(Button) view.findViewById(R.id.loginButton);
+        loginBtn.setOnClickListener(this);
+        /*signupButton =(Button)view.findViewById(R.id.signupButton);
+        signupButton.setOnClickListener(this);*/
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.loginButton:
+
+                break;
+        }
+
     }
 }
